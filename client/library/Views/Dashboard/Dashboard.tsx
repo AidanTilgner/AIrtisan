8ca2c9936cf10817050aae0781d40b3a150e644b
atilgner <aidantilgner02@gmail.com>
2023-05-09 00:28:30 -0700

    feat: started adding dark mode

--- client/library/Views/Dashboard/Dashboard.tsx
+++ client/library/Views/Dashboard/Dashboard.tsx
@@ -16,15 +16,15 @@ function Dashboard() {
       message = "Good morning!";
     }
     if (hours >= 12 && hours < 16) {
-      message = "Hi there, hope you're having a good day!";
+      message = "Hope you're having a good day!";
       icon = <Sun />;
     }
     if (hours >= 16 && hours < 19) {
-      message = "Good evening, welcome!";
+      message = "Good evening!";
       icon = <SunHorizon />;
     }
     if (hours >= 19) {
-      message = "Welcome, you're up late!";
+      message = "You're up late!";
       icon = <MoonStars />;
     }
     return {
@@ -40,6 +40,11 @@ function Dashboard() {
 
   const navigate = useNavigate();
 
+  const formattedDisplayName = (n: string) => {
+    const [first, ...rest] = n.split(" ");
+    return `${first}`;
+  };
+
   return (
     <div className={styles.Dashboard}>
       <div className={styles.top}>
@@ -47,7 +52,13 @@ function Dashboard() {
           {welcomeMessage.message} <WelcomeIcon />
         </p>
         <h1 className={styles.big_text}>
-          Hey <strong>{user?.display_name || user?.username}</strong>!
+          Hey{" "}
+          <strong>
+            {user?.display_name
+              ? formattedDisplayName(user.display_name)
+              : user?.username}
+          </strong>
+          !
         </h1>
         <div className={styles.quickActions}>
           <button
@@ -56,10 +67,16 @@ function Dashboard() {
           >
             New Bot
           </button>
+          <button
+            className={`${styles.quickAction} ${styles.btnSecondary}`}
+            onClick={() => navigate(`/profile/${user?.username}`)}
+          >
+            Your Profile
+          </button>
         </div>
       </div>
       <div className={styles.content}>
-        <SVG.UnderConstruction width="50%" height="50%" />
+        <SVG.UnderConstruction width="35%" height="35%" />
         <h2>This page is under construction. More is coming soon!</h2>
       </div>
     </div>
