7668bc9841a8e96fed32438e1b00dac0ed291d1e
atilgner <aidantilgner02@gmail.com>
2023-05-31 16:34:02 -0700

    feat: data should be cached

--- routes/data.ts
+++ routes/data.ts
@@ -3,6 +3,10 @@ import path from "path";
 
 const router = Router();
 
-router.use(Express.static(path.join("public", "data")));
+router.use(
+  Express.static(path.join("public", "data"), {
+    maxAge: 86400000,
+  })
+);
 
 export default router;
