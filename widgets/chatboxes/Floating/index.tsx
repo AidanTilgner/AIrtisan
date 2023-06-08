9f25a78b5bf119182ee85f419d2d8d6bdf05ce56
atilgner <aidantilgner02@gmail.com>
2023-06-07 17:54:36 -0700

    feat: some more fine tuning

--- widgets/chatboxes/Floating/index.tsx
+++ widgets/chatboxes/Floating/index.tsx
@@ -12,6 +12,32 @@ if (!domContainer()) {
   document.body.appendChild(newDomContainer);
 }
 
+// add google fonts
+const addGoogleFonts = () => {
+  //  <link rel="preconnect" href="https://fonts.googleapis.com">
+  {
+    /* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Quicksand:wght@300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">  */
+  }
+  const link = document.createElement("link");
+  link.rel = "preconnect";
+  link.href = "https://fonts.googleapis.com";
+  document.head.appendChild(link);
+
+  const link2 = document.createElement("link");
+  link2.rel = "preconnect";
+  link2.href = "https://fonts.gstatic.com";
+  link2.crossOrigin = "true";
+  document.head.appendChild(link2);
+
+  const link3 = document.createElement("link");
+  link3.href =
+    "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Quicksand:wght@300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap";
+  link3.rel = "stylesheet";
+  document.head.appendChild(link3);
+};
+addGoogleFonts();
+
 const AIrtisanSettings = (window as unknown as SettingsWindow).AIrtisanSettings;
 
 const addStyles = async () => {
